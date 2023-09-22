47c354f8997a4e9342724dd5ac050e82c4fa998c
yusufby <yusufbayram19891989@gmail.com>
2023-09-22 10:54:00 +0100

    convert to string or number

---
+++ 12.Javascript practice/convertInt.js
@@ -0,0 +1,4 @@
+var age="23";
+var age=+age;
+console.log(age);
+console.log(typeof(age));
---
+++ 12.Javascript practice/convertString.js
@@ -0,0 +1,4 @@
+var age=23;
+var string="Hello Yusuf. Your age is " +age;
+console.log(string);
+console.log(typeof(string));
