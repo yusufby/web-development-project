0a8b9eed8b09d252918a1c910d6d321139035a71
yusufby <yusufbayram19891989@gmail.com>
2023-09-25 22:45:06 +0100

    javascript practice

---
+++ 12.Javascript practice/oddNumbers LessThan100.js
@@ -0,0 +1,6 @@
+function oddNumber(){
+for(var i=1 ; i<=100 ; i+=2){
+      print(i +" ")
+   }
+}
+oddNumber();
---
+++ 12.Javascript practice/printNumbers.js
@@ -0,0 +1,7 @@
+
+function number(){
+for(var i=0; i<=10 ; i++){
+   console.log(i);
+}}
+
+number();
