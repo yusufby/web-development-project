db7a316815c4f632377b00368eacedbb3df71806
yusufby <yusufbayram19891989@gmail.com>
2023-09-25 22:22:13 +0100

    dice

--- Dicee Challenge - Starting Files/dice.js
+++ Dicee Challenge - Starting Files/dice.js
@@ -1,7 +1,23 @@
-function getRandom(){
-   var RandomNumber1 = Math.floor(Math.random() * 6) + 1;
-   var RandomNumber2 = Math.floor(Math.random() * 6) + 1;
- 
-   document.getElementById("diceImage1").innerHTML = '<img src="dice' + RandomNumber1 + '.png">';
-   document.getElementById("diceImage2").innerHTML = '<img src="dice' + RandomNumber2 + '.png">';
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src",
+    "dice" + randomNumber1 + ".png");
+
+document.querySelector(".img2").setAttribute("src",
+    "dice" + randomNumber2 + ".png");
+
+if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
 }
+
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML
+                    = (player2 + " WINS!");
+}
+
+else {
+    document.querySelector("h1").innerHTML
+                    = (player1 + " WINS!");
+}
+}, 2500);
