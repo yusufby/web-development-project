829bea3d33a8928387231eaa5cbd8883491cb96a
yusufby <yusufbayram19891989@gmail.com>
2023-09-28 03:01:43 +0100

    CelsiustoFahrenheit

---
+++ 12.Javascript practice/Celsius-to-Fahrenheit.js
@@ -0,0 +1,7 @@
+function CelsiustoFahrenheit(n) {
+  var celsius = n;
+  var fahrenheit = (celsius * 9) / 5 + 32;
+  console.log("fahrenheit is :"+fahrenheit);
+  console.log("celsius is :"+celsius);
+}
+CelsiustoFahrenheit(114);
---
+++ 12.Javascript practice/sumOfEvenNumbers.js
@@ -0,0 +1,11 @@
+
+function sumNumber(){
+   var sumOfEvenNumber=0;
+   for(var i=11 ; i<30 ;i++){
+      if(i%2==0){
+         sumOfEvenNumber+=i;
+      }
+      console.log(sumOfEvenNumber)
+   }
+}
+sumNumber();
